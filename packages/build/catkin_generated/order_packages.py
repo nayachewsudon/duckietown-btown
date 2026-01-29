3e668f76f2596d579be81fa67b3e6932d840b537
chits-nema <cvnemazuwa@gmail.com>
2026-01-29 17:12:18 +0000

    add drive straight node and launch file

---
+++ packages/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/code/catkin_ws/src/duckietown-btown/packages/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/code/catkin_ws/devel;/opt/ros/noetic'.split(';') if '/code/catkin_ws/devel;/opt/ros/noetic' != '' else []
---
+++ packages/build/gtest/googlemock/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /usr/src/googletest/googlemock
+# Build directory: /code/catkin_ws/src/duckietown-btown/packages/build/gtest/googlemock
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("../googletest")
