338349622cd511edb9ad33f32758c81088f16286
chits-nema <cvnemazuwa@gmail.com>
2026-01-31 15:35:48 +0000

    driving straight node completed uses the keyboard control to run depndant nodes

---
+++ packages/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /code/catkin_ws/src/duckietown-btown/packages/src
+# Build directory: /code/catkin_ws/src/duckietown-btown/packages/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
