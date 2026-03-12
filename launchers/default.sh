55eefb558592c032c1ef8a1585eee4d4027efc32
chits-nema <cvnemazuwa@gmail.com>
2026-03-12 21:10:39 +0100

    FIX: changed launch file for project build

--- launchers/default.sh
+++ launchers/default.sh
@@ -12,20 +12,8 @@ dt-launchfile-init
 # NOTE: Use the variable DT_REPO_PATH to know the absolute path to your code
 # NOTE: Use `dt-exec COMMAND` to run the main process (blocking process)
 
-# launching all nodes required by FSM
-dt-exec bash -c "
-  roslaunch anti_instagram anti_instagram_node.launch veh:=\$VEHICLE_NAME &
-  roslaunch line_detector line_detector_node.launch veh:=\$VEHICLE_NAME &
-  roslaunch lane_filter lane_filter_node.launch veh:=\$VEHICLE_NAME &
-  roslaunch ground_projection ground_projection_node.launch veh:=\$VEHICLE_NAME &
-  roslaunch lane_control lane_control_node.launch veh:=\$VEHICLE_NAME &
-  roslaunch led_emitter led_emitter_node.launch veh:=\$VEHICLE_NAME &
-  roslaunch led_joy_mapper led_joy_mapper_node.launch veh:=\$VEHICLE_NAME &
-  
-  # Launch FSM last (it controls the other nodes via services)
-  roslaunch fsm fsm_node.launch veh:=\$VEHICLE_NAME
-"
-
+# launching duckietown lane following demo to launch all the nodes 
+dt-exec roslaunch --wait duckietown_demos lane_following.launch veh:=\$VEHICLE_NAME
 
 # ----------------------------------------------------------------------------
 # YOUR CODE ABOVE THIS LINE
