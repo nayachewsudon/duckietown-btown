9ac7df7bf1c62828b81d48e0e3d143d63bf6af78
chits-nema <cvnemazuwa@gmail.com>
2026-03-12 15:44:53 +0100

    IMP: default.sh roslaunch nodes activated

--- launchers/default.sh
+++ launchers/default.sh
@@ -12,8 +12,19 @@ dt-launchfile-init
 # NOTE: Use the variable DT_REPO_PATH to know the absolute path to your code
 # NOTE: Use `dt-exec COMMAND` to run the main process (blocking process)
 
-# launching app
-dt-exec echo "This is an empty launch script. Update it to launch your application."
+# launching all nodes required by FSM
+dt-exec bash -c "
+  roslaunch anti_instagram anti_instagram_node.launch veh:=\$VEHICLE_NAME &
+  roslaunch line_detector line_detector_node.launch veh:=\$VEHICLE_NAME &
+  roslaunch lane_filter lane_filter_node.launch veh:=\$VEHICLE_NAME &
+  roslaunch ground_projection ground_projection_node.launch veh:=\$VEHICLE_NAME &
+  roslaunch lane_control lane_control_node.launch veh:=\$VEHICLE_NAME &
+  roslaunch led_emitter led_emitter_node.launch veh:=\$VEHICLE_NAME &
+  roslaunch led_joy_mapper led_joy_mapper_node.launch veh:=\$VEHICLE_NAME &
+  
+  # Launch FSM last (it controls the other nodes via services)
+  roslaunch fsm fsm_node.launch veh:=\$VEHICLE_NAME
+"
 
 
 # ----------------------------------------------------------------------------
